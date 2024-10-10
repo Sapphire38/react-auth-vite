82c67d199f438557aca0e4b88ebd944fad4dcb1e
Juani <juanygr99@gmail.com>
2024-10-10 00:01:21 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,165 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import "./App.css";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
 
 function App() {
-  const menuItems = [
-    { key: "1", label: <Link to="/">Home</Link> },
-    { key: "2", label: <Link to="/profile">Profile</Link> },
+  const items = [
+    {
+      key: "sub1",
+      label: "Navigation One",
+      icon: <MailOutlined />,
+      children: [
+        {
+          key: "g1",
+          label: "Item 1",
+          type: "group",
+          children: [
+            {
+              key: "1",
+              label: "Option 1",
+            },
+            {
+              key: "2",
+              label: "Option 2",
+            },
+          ],
+        },
+        {
+          key: "g2",
+          label: "Item 2",
+          type: "group",
+          children: [
+            {
+              key: "3",
+              label: "Option 3",
+            },
+            {
+              key: "4",
+              label: "Option 4",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      key: "sub2",
+      label: "Navigation Two",
+      icon: <AppstoreOutlined />,
+      children: [
+        {
+          key: "5",
+          label: "Option 5",
+        },
+        {
+          key: "6",
+          label: "Option 6",
+        },
+        {
+          key: "sub3",
+          label: "Submenu",
+          children: [
+            {
+              key: "7",
+              label: "Option 7",
+            },
+            {
+              key: "8",
+              label: "Option 8",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: "divider",
+    },
+    {
+      key: "sub4",
+      label: "Navigation Three",
+      icon: <SettingOutlined />,
+      children: [
+        {
+          key: "9",
+          label: "Option 9",
+        },
+        {
+          key: "10",
+          label: "Option 10",
+        },
+        {
+          key: "11",
+          label: "Option 11",
+        },
+        {
+          key: "12",
+          label: "Option 12",
+        },
+      ],
+    },
+    {
+      key: "sub5",
+      label: "Navigation Two",
+      icon: <AppstoreOutlined />,
+      children: [
+        {
+          key: "5",
+          label: "Option 5",
+        },
+        {
+          key: "6",
+          label: "Option 6",
+        },
+        {
+          key: "sub3",
+          label: "Submenu",
+          children: [
+            {
+              key: "7",
+              label: "Option 7",
+            },
+            {
+              key: "8",
+              label: "Option 8",
+            },
+          ],
+        },
+      ],
+    },
   ];
+
+  const onClick = (e) => {
+    console.log("click ", e);
+  };
 
   return (
     <div style={{ padding: "0" }}>
-      <div className=" ">
-        <div className="flex flex-col gap-2">
-          
-          <Link to="/">Hola</Link>
-          <Link to="/profile">Profile</Link>
+      <div className="flex w-screen h-screen overflow-hidden bg-neutral-900">
+        <div className="flex w-ful lg:w-1/5 h-screen overflow-y-scroll scrollbar">
+          <Menu
+            onClick={onClick}
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            mode="inline"
+            items={items}
+          />
         </div>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
+        <div className="flex w-4/5">
+      
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
